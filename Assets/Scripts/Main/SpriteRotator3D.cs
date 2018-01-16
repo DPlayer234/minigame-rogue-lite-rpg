bf2c221939261719063bd85a2909c0e9313376bf
DPlayer234 <dplayer.235@gmail.com>
2018-01-16 16:30:50 +0100

    Basic Sprite Objects for 3D
    ... feels like I'm re-inventing the wheel.

---
+++ Assets/Scripts/Main/SpriteRotator3D.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SAE.RougePG
+{
+    /// <summary>
+    ///     Will make a <seealso cref="Sprite"/> rotate towards the set <seealso cref="Camera"/>.
+    ///     Also makes sure that all Sprites have the correct order within the 3D space.
+    /// </summary>
+    public class SpriteRotator3D : MonoBehaviour
+    {
+        /// <summary> The camera to rotate to. Defaults to <see cref="StateManager.MainCamera"/>. </summary>
+        public Camera mainCamera;
+
+        /// <summary> Contains all sprite renderers in this <seealso cref="GameObject"/> children. </summary>
+        private SpriteRenderer[] spriteRenderers;
+
+        /// <summary> Multiplier for sorting order </summary>
+        private const float SortingOrderMultiplier = 1000.0f;
+
+        /// <summary>
+        ///     Called by Unity to initialize the <seealso cref="SpriteRotator3D"/> when it first becomes active
+        /// </summary>
+        private void Start()
+        {
+            if (mainCamera == null)
+            {
+                // Default to the main camera of the scene
+                mainCamera = StateManager.MainCamera;
+            }
+
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+
+        /// <summary>
+        ///     Called by Unity once every frame after all Updates and FixedUpdates have been executed.
+        /// </summary>
+        private void LateUpdate()
+        {
+            // Face Camera
+            this.transform.rotation = Quaternion.Euler(0.0f, mainCamera.transform.rotation.eulerAngles.y, 0.0f);
+
+            // Set Layer Order => Get Distance to camera, add int.MinValue to increase overall range and cap the value at int.MaxValue
+            Vector3 distances = this.transform.position - mainCamera.transform.position;
+
+            int sortingOrder = -(int)Mathf.Min(int.MaxValue,
+                SortingOrderMultiplier * (distances.x * distances.x + distances.y * distances.y + distances.z * distances.z) + int.MinValue);
+
+            foreach (var renderer in spriteRenderers)
+            {
+                renderer.sortingOrder = sortingOrder;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Main/StateManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SAE.RougePG
+{
+    /// <summary>
+    ///     Stores and manages general game state.
+    ///     Behaves like a singleton; any new instance will override the old one.
+    /// </summary>
+    public class StateManager : MonoBehaviour
+    {
+        [SerializeField]
+        /// <summary> The main camera in the scene. To be set from the UnityEditor. </summary>
+        private Camera mainCamera;
+
+        /// <summary>
+        ///     The global instance of the <see cref="StateManager"/>.
+        /// </summary>
+        private static StateManager instance;
+
+        /// <summary>
+        ///     The main camera in the scene.
+        /// </summary>
+        public static Camera MainCamera { private set; get; }
+
+        /// <summary>
+        ///     Called by Unity to initialize the <seealso cref="StateManager"/> whether it is or is not active.
+        /// </summary>
+        private void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(instance);
+            }
+
+            instance = this;
+            DontDestroyOnLoad(this);
+
+            // Copy relevant set fields.
+            MainCamera = mainCamera;
+        }
+    }
+}
